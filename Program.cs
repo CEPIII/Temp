de2e0f3739e8ea84193d92219742ad4f05047971
CEPIII <seroboy@yandex.ru>
2020-12-07 08:45:08 +0300

    Создал двумерный массив и заполнил рандомными значениями

--- Program.cs
+++ Program.cs
@@ -7,13 +7,29 @@ namespace Temp
     {
         static void Main(string[] args)
         {
-            /// Подсчитать сумму двумерного массива
-            /// Найти минимальный элемент массива
-            /// </summary>
+            Console.Write("Введите положительный диапазон значений случайных чисел, которые заполнят массив от 0 \n \tdiapazon = ");
+            int diapazon = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nОпределите первый размер двумерного массива : \n\t[x, ] = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nОпределите второй размер двумерного массива : \n\t[, y] = ");
+            int y = Convert.ToInt32(Console.ReadLine());
 
-            int[,] arr = new int[,] { { 11, 22, 31 }, { 4, 53, 6 }, { 7, 81, 90 } };
-            int rank = arr.Rank;
-            Console.WriteLine(rank);
+            int[,] arr = new int[x, y];
+            //Заполнить массив рандомными значениями по заданному ранее диапазону
+            Random rnd = new Random();
+            //Заодно найду максимальный элемент, минимальный и сумму всех элементов
+            int min, max;
+            Console.WriteLine($"\nЗаполненный массив \n\t int[,] arr = new int[{x}, {y}]\n\n");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.Write("{");
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = rnd.Next(0, diapazon);
+                    Console.Write($"  {arr[i, j]}  ");
+                }
+                Console.WriteLine("}\n");
+            }
         }
     }
 }
