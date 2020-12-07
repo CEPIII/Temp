df16cd93b950a37b1661e9546e53288b2ade8e12
CEPIII <seroboy@yandex.ru>
2020-12-07 08:29:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Подсчитать сумму двумерного массива
+            /// Найти минимальный элемент массива
+            /// </summary>
+
+            int[,] arr = new int[,] { { 11, 22, 31 }, { 4, 53, 6 }, { 7, 81, 90 } };
+            int rank = arr.Rank;
+            Console.WriteLine(rank);
+        }
+    }
+}
+
