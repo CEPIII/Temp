196b4cc78eaf037c7b577e3596477462b51292b4
CEPIII <seroboy@yandex.ru>
2020-12-14 17:54:31 +0300

    Синхронизировал с ГитХабом

--- Program.cs
+++ Program.cs
@@ -30,6 +30,38 @@ namespace Temp
                 }
                 Console.WriteLine("}\n");
             }
+
+            //Заодно найду максимальный элемент, минимальный и сумму всех элементов
+            int min = arr[0, 0];
+            int max = arr[0, 0];
+            int summ = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    summ += arr[i, j];      //Подсчет Суммы элементов
+
+                    if (min > arr[i, j]) min = arr[i, j];
+                    if (max < arr[i, j]) max = arr[i, j];
+                }
+            }
+
+            Console.WriteLine($"Наименьший элемент массива \n\tmin = {min}\n");
+            Console.WriteLine($"Наибольший элемент массива \n\tmax = {max}\n");
+            Console.WriteLine($"Сумма элементов массива \n\tsumm = {summ}\n");
+
+            //Узнаю общее количество элементов двумерного массива
+            int lengthArr = arr.Length;
+            Console.WriteLine("Количество элементов двумерного массива = " + lengthArr);
+            //Создаю одномерный универсальный массив принимающий значения вышеполученного многомерного 
+            int[] arrFromTwin = new int[lengthArr];
+            //Заполняю
+            /*
+            int x = 0;
+            foreach (var item in arr)
+            {
+
+            }*/
         }
     }
 }
