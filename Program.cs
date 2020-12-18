e698e078118efd8cee9d125d729aa4c8604223d2
CEPIII <seroboy@yandex.ru>
2020-12-18 14:52:29 +0300

    Программа завершена

--- Program.cs
+++ Program.cs
@@ -17,8 +17,7 @@ namespace Temp
             int[,] arr = new int[x, y];
             //Заполнить массив рандомными значениями по заданному ранее диапазону
             Random rnd = new Random();
-            //Заодно найду максимальный элемент, минимальный и сумму всех элементов
-            int min, max;
+ 
             Console.WriteLine($"\nЗаполненный массив \n\t int[,] arr = new int[{x}, {y}]\n\n");
             for (int i = 0; i < arr.GetLength(0); i++)
             {
@@ -32,8 +31,8 @@ namespace Temp
             }
 
             //Заодно найду максимальный элемент, минимальный и сумму всех элементов
-            int mini = arr[0, 0];
-            int maxi = arr[0, 0];
+            int min = arr[0, 0];
+            int max = arr[0, 0];
             int summ = 0;
             for (int i = 0; i < arr.GetLength(0); i++)
             {
@@ -41,32 +40,37 @@ namespace Temp
                 {
                     summ += arr[i, j];      //Подсчет Суммы элементов
 
-                    if (mini > arr[i, j]) mini = arr[i, j];
-                    if (maxi < arr[i, j]) maxi = arr[i, j];
+                    if (min > arr[i, j]) min = arr[i, j];
+                    if (max < arr[i, j]) max = arr[i, j];
                 }
             }
 
-            Console.WriteLine($"Наименьший элемент массива \n\tmin = {mini}\n");
-            Console.WriteLine($"Наибольший элемент массива \n\tmax = {maxi}\n");
+            Console.WriteLine($"Наименьший элемент массива \n\tmin = {min}\n");
+            Console.WriteLine($"Наибольший элемент массива \n\tmax = {max}\n");
             Console.WriteLine($"Сумма элементов массива \n\tsumm = {summ}\n");
 
             //Узнаю общее количество элементов двумерного массива
             int lengthArr = arr.Length;
             Console.WriteLine("Количество элементов двумерного массива = " + lengthArr);
+            Console.WriteLine();
             //Создаю одномерный универсальный массив принимающий значения вышеполученного многомерного 
             int[] arrFromTwin = new int[lengthArr];
 
             //Заполняю и вывожу на экран
-            
+            Console.WriteLine("Вывожу на экран каждый элемент одномерного массива:");
             int xTemp = 0;
             foreach (int item in arr)
             {
                 arrFromTwin[xTemp] = item;
                 Console.Write(arrFromTwin[xTemp] + ", ");
                 xTemp++;
-
+                /*
+                if (xTemp == lengthArr)
+                {
+                    Console.Write("\b\b \n");       //Затер последнюю запятую после значений
+                }*/
             }
-
+            Console.WriteLine("\b\b \n");
             
         }
     }
